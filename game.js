8df3d3fb4f77f4e047233924eda59fdb3bee7d77
black9509234 <mobile7mo@gmail.com>
2025-04-18 13:57:08 +0900

    .

---
+++ game.js
@@ -0,0 +1,52 @@
+let number = 1
+let n1 = 0;
+let n1_producing = 1
+let n1_cost = 1;
+let n2_cost = 500;
+let n2_producing = 1;
+document.getElementById('number').innerText = number;
+document.getElementById('n1_producing').innerText = n1_producing;
+document.getElementById('n1_cost').innerText = n1_cost;
+document.getElementById('n1').innerText = n1;
+document.getElementById('n2_cost').innerText = n2_cost;
+document.getElementById('n2_producing').innerText = n2_producing;
+
+function IncreaseN1() {
+  if (number >= n1_cost){
+    number -= n1_cost;
+    n1++;
+    n1_cost *= 1.25;
+  }
+
+  document.getElementById('n1').innerText = n1;
+  if (n1_cost >= 100) {
+    document.getElementById('n1_cost').innerText = Math.floor(n1_cost);
+  } else {
+    document.getElementById('n1_cost').innerText = n1_cost.toFixed(2);
+  };
+}
+
+
+function FunctionN2(){
+  if (number >= n2_cost){
+    number -= n2_cost;
+    n2_producing *= 1.15;
+    n1_producing *= 1.15;
+    n2_cost *= 1.25;
+  }
+
+  document.getElementById('n2_producing').innerText = n2_producing;
+  document.getElementById('n2_cost').innerText = n2_cost;
+  document.getElementById('n1_producing').innerText = n1_producing;
+}
+
+
+setInterval(function() {
+  number += n1 * n1_producing / 10;
+
+  if (number >= 100) {
+    document.getElementById('number').innerText = Math.floor(number);
+  } else {
+    document.getElementById('number').innerText = number.toFixed(2);
+  }
+}, 100);
