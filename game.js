97aa453f30a45cd22f01c5cb794177662870f2f5
black9509234 <mobile7mo@gmail.com>
2025-04-19 22:50:37 +0900

    .

--- game.js
+++ game.js
@@ -72,11 +72,11 @@ function DecreaseN2() {
 
 
 setInterval(function() {
-  number += n1 * n1_producing * 10;
+  number += n1 * n1_producing / 10;
 
   if (number >= 100) {
     document.getElementById('number').innerText = Math.floor(number);
   } else {
     document.getElementById('number').innerText = number.toFixed(2);
   }
-}, 1);
+}, 100);
